00da0c7332523e2cd63f3babd7eb8a71f0e64377
Maria Zubova <73553427+mszubova@users.noreply.github.com>
2021-11-03 04:53:29 +0500

    Add files via upload

---
+++ BlackBoxTesting/BlackBoxTesting/ExpressionCalculator.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BlackBoxTesting
+{
+    public static class ExpressionCalculator
+    {
+        public static double Calculate(double x1, double x2, double x3, double x4)
+        {
+            return Math.Abs(x2 - x1) + Math.Abs(x4 - x3);
+        }
+        /// <summary>
+        /// Скалярное произведение вектора
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns> 
+        public static double GetSkalyar(Point v1, Point v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y;
+        }
+        /// <summary>
+        /// Расстояние между точками
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double GetLenght(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        }
+        /// <summary>
+        /// Находит вектор прямой заданной двумя точками
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Point GetVector(Point a, Point b)
+        {
+            return new Point(a.X - b.X, a.Y - b.Y);
+        }
+        /// <summary>
+        /// Проверка на коллинеарность векторов
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool IsColleniar(Point v1, Point v2)
+        {
+            if (v1.Y == 0 || v2.Y == 0)
+                return v1.Y / v1.X == v2.Y / v2.X;
+            else
+                return v1.X / v1.Y == v2.X / v2.Y;
+        }
+        /// <summary>
+        /// Проверка на ромб
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsRhomb(Point a, Point b, Point c, Point d)
+        {
+            double ab = GetLenght(a,b), bc = GetLenght(b, c), cd = GetLenght(c, d), da = GetLenght(d, a);
+            return ab == bc && bc == cd && cd == da;
+        }
+        /// <summary>
+        /// Проверка на квадрат
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsSquare(Point a, Point b, Point c, Point d)
+        {
+            return IsRectangle(a, b, c, d) && IsRhomb(a, b, c, d);
+        }
+        /// <summary>
+        /// Проверка на парралелограм
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsParallelogram(Point a, Point b, Point c, Point d)
+        {
+            Point v1 = GetVector(a, b);
+            Point v2 = GetVector(c, d);
+            Point v3 = GetVector(b, c);
+            Point v4 = GetVector(d, a);
+            return IsColleniar(v1, v2) && IsColleniar(v3, v4);
+        }
+        /// <summary>
+        /// Проверка на прямоугольник
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsRectangle(Point a, Point b, Point c, Point d)
+        {
+            Point v1 = GetVector(a, b);
+            Point v2 = GetVector(c, d);
+            Point v3 = GetVector(b, c);
+            Point v4 = GetVector(d, a);
+            return IsColleniar(v1, v2) && IsColleniar(v3, v4) && GetSkalyar(v1, v3) == 0 && GetSkalyar(v2, v4) == 0;
+        }
+
+        /// <summary>
+        /// Проверка на трапецию
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsTrapezoid(Point a, Point b, Point c, Point d)
+        {
+            Point v1 = GetVector(a, b);
+            Point v2 = GetVector(b, c);
+            Point v3 = GetVector(c, d);
+            Point v4 = GetVector(d, a);
+            return (IsColleniar(v1, v3) && !IsColleniar(v2, v4)) || (!IsColleniar(v1, v3) && IsColleniar(v2, v4));
+        }
+        /// <summary>
+        /// Проверка на равнобедренную трапецию
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsIsoscelesTrapezoid(Point a, Point b, Point c, Point d)
+        {
+            return (GetLenght(b, c) == GetLenght(d, a) || GetLenght(a, b) == GetLenght(c, d)) && IsTrapezoid(a, b, c, d);
+        }
+        /// <summary>
+        /// Проверка на прямоугольную трапецию
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool IsRectangularTrapezoid(Point a, Point b, Point c, Point d)
+        {
+            Point v1 = GetVector(a, b);
+            Point v2 = GetVector(b, c);
+            Point v3 = GetVector(c, d);
+            Point v4 = GetVector(d, a);
+            return IsTrapezoid(a, b, c, d) && (GetSkalyar(v1, v2) == 0 || GetSkalyar(v3, v4) == 0);
+        }
+    }
+}
---
+++ BlackBoxTesting/BlackBoxTesting/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlackBoxTesting
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnRes2_Click(object sender, RoutedEventArgs e)
+        {
+            Point p1;
+            Point p2;
+            Point p3;
+            Point p4;
+            try
+            {
+                p1 = new Point(double.Parse(TxbP1x.Text), double.Parse(TxbP1y.Text));
+                p2 = new Point(double.Parse(TxbP2x.Text), double.Parse(TxbP2y.Text));
+                p3 = new Point(double.Parse(TxbP3x.Text), double.Parse(TxbP3y.Text));
+                p4 = new Point(double.Parse(TxbP4x.Text), double.Parse(TxbP4y.Text));
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+            if (ExpressionCalculator.IsSquare(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Квадрат";
+            }
+            else if (ExpressionCalculator.IsRectangle(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Прямоугольник";
+            }
+            else if (ExpressionCalculator.IsRhomb(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Ромб";
+            }
+            else if (ExpressionCalculator.IsParallelogram(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Параллелограм";
+            }
+            else if (ExpressionCalculator.IsIsoscelesTrapezoid(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Равнобедренная трапеция";
+            }
+            else if (ExpressionCalculator.IsRectangularTrapezoid(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Прямоугольная трапеция";
+            }
+            else if (ExpressionCalculator.IsTrapezoid(p1, p2, p3, p4))
+            {
+                TxbResult2.Text = "Трапеция общего вида";
+            }
+            else
+            {
+                TxbResult2.Text = "Четырехугольник общего вида";
+            }
+        }
+
+        private void BtnRes1_Click(object sender, RoutedEventArgs e)
+        {
+            double x1, x2, x3, x4;
+
+            if (!double.TryParse(TxbS1x1.Text, out x1))
+            {
+                string t = "";
+                for (int i = 0; i < TxbS1x1.Text.Length; i++)
+                {
+                    if (char.IsDigit(TxbS1x1.Text[i]))
+                    {
+                        t += TxbS1x1.Text[i];
+                    }
+                }
+                double.TryParse(t, out x1);
+            }
+            double.TryParse(TxbS1x2.Text, out x2);
+            double.TryParse(TxbS2x1.Text, out x3);
+            double.TryParse(TxbS2x2.Text, out x4);
+
+            TxbResult1.Text = $"Сумма теней = {ExpressionCalculator.Calculate(x1, x2, x3, x4)}";
+        }
+
+        private void Txb_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key < Key.D0 || e.Key > Key.D9) && e.Key != Key.OemMinus)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TxbA_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key <= Key.D0 || e.Key >= Key.D9) && e.Key != Key.OemMinus)
+            {
+                e.Handled = true;
+            }
+            if (e.Key >= Key.A && e.Key <= Key.Z)
+                e.Handled = false;
+        }
+    }
+}
